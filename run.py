66e782d00b9491552743c0e7e653b1c0ce4a2b4c
Nithin Mahesh <nmahesh@aras.com>
2020-12-06 02:43:09 -0500

    Grabbing ItemTypes and Properties then querying for ID data then prior property lengths

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 import pandas
 import os
-import sqlite3
-
+import pyodbc 
+import re
+import string
 
 def main():
     reading()
@@ -11,7 +12,8 @@ def reading():
     path = os.getcwd() + "\Width Lengths.xlsx"
     data = pandas.read_excel(path, sheet_name='Sheet1')
 
-    itemtype = {}
+
+    items = {}
     for index, row in data.iterrows():
         key = row['ItemType']
         value = row['Width']
@@ -27,27 +29,84 @@ def reading():
 
         if key == "Finish":
             boolean = False
-            query(itemtype)
+            query(items)
             continue
 
         concat = key, value
 
         if check1 == False and check2 == True:
-            itemtype[key] = value
+            items[key] = value
             boolean = True
            
         if boolean == True:
-            itemtype[key] = value    
+            items[key] = value    
 
-
-        print(concat)
-        if(index == 790):
-            print(itemtype)
     
 
-def query(itemtype):
-    print(itemtype)
-    itemtype.clear()
+def query(items):
+
+    conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=\SQLEXPRESS;'
+                      'Database=12_SP11;'
+                      'Trusted_Connection=yes;')
+
+    cursor = conn.cursor()
+    count = 0
+    itemProperty = []
+    # itemPropertyValue = []
+    for key,value in items.items():
+        
+        if len(key) > 0 and pandas.isnull(value):
+            itemtype = key
+        
+        # if key and value are not null then save properties
+        if len(key) > 0 and not pandas.isnull(value):
+            # count = count + 1
+            itemProperty.append(key)
+            itemProperty.append(value)
+            # itemPropertyValue.append(value)
+            # if count > 1:
+            #     itemProperty.append(key)
+            #     itemPropertyValue.append(value)
+
+            # if its count is greater than 1 then add that property
+            
+
+        print(key,value)
+
+    # Get itemtypes source ID first 
+    querySource_ID = "SELECT ID FROM innovator.ITEMTYPE WHERE NAME='" +itemtype+ "'"
+    print(querySource_ID)
+
+    cursor.execute(querySource_ID)
+
+    # Grab the ID from response
+    for row in cursor:
+        row = str(row).split("'",1)
+        row = row[1]
+        row = row.split("',",1)
+        ID = row[0]
+
+        print(ID)
+
+    # Query for old width lenghts and save those before running change query
+    # SELECT COLUMN_WIDTH FROM innovator.PROPERTY WHERE name='property name' AND SOURCE_ID='the id we got'
+    for properties in itemProperty:
+        size = len(itemProperty)
+        if size == 2:
+            queryOldLengths = "SELECT COLUMN_WIDTH FROM innovator.PROPERTY WHERE NAME=" + "'" +properties[0]+ "'" " AND SOURCE_ID=" +"'" +ID+ "'"
+            cursor.execute(queryOldLengths)
+        
+        # There are more than 1 property values
+        queryOldLengths = "SELECT COLUMN_WIDTH FROM innovator.PROPERTY WHERE NAME=" + "'" +properties+ "'" " AND SOURCE_ID=" +"'" +ID+ "'"
+        cursor.execute(queryOldLengths)
+        for row in cursor:
+            print(row)
+        # print("true")
+    
+    # Setup second query using source ID and property values 
+
+    items.clear()
 
 
 main()
