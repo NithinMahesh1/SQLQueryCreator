17a25d77b88f4e33a88840fc05a024283c17c983
Nithin Mahesh <nmahesh@aras.com>
2020-12-06 22:12:18 -0500

    Queries old width lengths then prints Itemtype and old property names/width lenghts. Also creates new query

--- run.py
+++ run.py
@@ -3,6 +3,12 @@ import os
 import pyodbc 
 import re
 import string
+import xlwt 
+from xlwt import Workbook 
+import xlsxwriter
+
+import openpyxl
+from openpyxl import load_workbook
 
 def main():
     reading()
@@ -12,6 +18,8 @@ def reading():
     path = os.getcwd() + "\Width Lengths.xlsx"
     data = pandas.read_excel(path, sheet_name='Sheet1')
 
+    sheet2 = load_workbook('Width Lengths.xlsx')
+    sheet2.create_sheet('Sheet2')
 
     items = {}
     for index, row in data.iterrows():
@@ -29,7 +37,7 @@ def reading():
 
         if key == "Finish":
             boolean = False
-            query(items)
+            query(items,sheet2)
             continue
 
         concat = key, value
@@ -43,7 +51,7 @@ def reading():
 
     
 
-def query(items):
+def query(items,sheet2):
 
     conn = pyodbc.connect('Driver={SQL Server};'
                       'Server=\SQLEXPRESS;'
@@ -53,24 +61,17 @@ def query(items):
     cursor = conn.cursor()
     count = 0
     itemProperty = []
-    # itemPropertyValue = []
     for key,value in items.items():
-        
+        propertyName = []
         if len(key) > 0 and pandas.isnull(value):
             itemtype = key
         
         # if key and value are not null then save properties
         if len(key) > 0 and not pandas.isnull(value):
-            # count = count + 1
             itemProperty.append(key)
-            itemProperty.append(value)
-            # itemPropertyValue.append(value)
-            # if count > 1:
-            #     itemProperty.append(key)
-            #     itemPropertyValue.append(value)
-
-            # if its count is greater than 1 then add that property
-            
+            # add name here
+            # propertyName = key
+            itemProperty.append(value)  
 
         print(key,value)
 
@@ -89,20 +90,67 @@ def query(items):
 
         print(ID)
 
+    count = 0
     # Query for old width lenghts and save those before running change query
     # SELECT COLUMN_WIDTH FROM innovator.PROPERTY WHERE name='property name' AND SOURCE_ID='the id we got'
     for properties in itemProperty:
+        count = count + 1
+        wks = sheet2['Sheet2']
+        wks.cell(row=1, column=1).value = itemtype
         size = len(itemProperty)
+        index = size/2
         if size == 2:
             queryOldLengths = "SELECT COLUMN_WIDTH FROM innovator.PROPERTY WHERE NAME=" + "'" +properties[0]+ "'" " AND SOURCE_ID=" +"'" +ID+ "'"
             cursor.execute(queryOldLengths)
+
+            for row in cursor:
+                row = str(row).split(",",1)
+                row = row[0].split("(",1)
+                row = int(row[1])
+                wks = sheet2['Sheet2']
+           
+                wks.cell(row=count, column=1).value = row
+                sheet2.save('Width Lengths.xlsx')
+                count = count - 1
+
+                print(row)
+
         
         # There are more than 1 property values
-        queryOldLengths = "SELECT COLUMN_WIDTH FROM innovator.PROPERTY WHERE NAME=" + "'" +properties+ "'" " AND SOURCE_ID=" +"'" +ID+ "'"
-        cursor.execute(queryOldLengths)
-        for row in cursor:
-            print(row)
-        # print("true")
+        
+        if not isinstance(properties, int):
+            propertyName = properties
+
+            queryOldLengths = "SELECT COLUMN_WIDTH FROM innovator.PROPERTY WHERE NAME=" + "'" +properties+ "'" " AND SOURCE_ID=" +"'" +ID+ "'"
+            cursor.execute(queryOldLengths)
+            
+            for row in cursor:
+                row = str(row).split(",",1)
+                row = row[0].split("(",1)
+                row = int(row[1])
+
+                wks.cell(row=count, column=2).value = properties
+           
+                wks.cell(row=count, column=4).value = row
+                sheet2.save('Width Lengths.xlsx')
+                count = count - 1
+
+                print(row)
+
+        widthLength = 0
+        if isinstance(properties, int):
+            widthLength = properties
+        
+        # UPDATE innovator.PROPERTY SET COLUMN_WIDTH='new int width' WHERE NAME='property name' AND SOURCE_ID='ID variable'
+        
+        if not widthLength == 0 and not propertyName == "":
+            queryOldLengths = "UPDATE innovator.PROPERTY SET COLUMN_WIDTH=" + "'" +str(widthLength)+ "'" + " WHERE NAME=" + "'" +propertyName+ "'" +" AND SOURCE_ID=" + "'" +ID+ "'"
+            cursor.execute(queryOldLengths)
+
+            for row in cursor:
+                print(row)
+        
+        
     
     # Setup second query using source ID and property values 
 
